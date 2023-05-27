55b848e94c1ad761b9705648ade40d8a7450ab71
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-05-27 17:20:54 +0300

    Fixed user loading from ls on refresh

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 import MainLayout from './layouts/MainLayout';
 import MyPosts from './pages/MyPosts';
 import Post from './pages/Post';
@@ -12,36 +12,31 @@ export const UserContext = createContext(undefined);
 export const USER_KEY = 'user';
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(getFromLocalStorage(USER_KEY));
   const value = { user: user, setUser: setUser };
-
-  useEffect(() => {
-    const user = getFromLocalStorage(USER_KEY);
-    setUser(user);
-  }, [])
 
   return (
     <Routes>
       <Route
-        path='/'
+        path="/"
         element={
           <UserContext.Provider value={value}>
             <MainLayout />
           </UserContext.Provider>
         }>
-        <Route path='/' element={<AllPosts />} />
-        <Route path='/user/:userId/post' element={<MyPosts />} />
+        <Route path="/" element={<AllPosts />} />
+        <Route path="/user/:userId/post" element={<MyPosts />} />
         <Route path={'/user/:userId/post/:postId'} element={<Post />} />
       </Route>
       <Route
-        path='/login'
+        path="/login"
         element={
           <UserContext.Provider value={value}>
             <Login />
           </UserContext.Provider>
         }
       />
-      <Route path='/register' element={<Register />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
