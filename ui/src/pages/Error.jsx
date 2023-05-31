20def41033ffbd47bfaba2fe8fbdab3e3dc36df9
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-05-31 12:23:23 +0300

    Adjusted Error page

--- ui/src/pages/Error.jsx
+++ ui/src/pages/Error.jsx
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 
 const Error = () => {
-  return <ErrorWrapper>Oops...Error. Please try again later</ErrorWrapper>;
+  return (
+    <ErrorWrapper>
+      <div>Oops...</div>
+      <div>An error occurred.</div>
+      <div>Please try again later.</div>
+    </ErrorWrapper>
+  );
 };
 
-const ErrorWrapper = styled.div`
+const ErrorWrapper = styled.div.attrs({
+  className: 'flex-column',
+})`
+  justify-content: center;
+  align-content: center;
   font-size: 15px;
   padding: 40px;
+  margin: 40px;
+  
+  div {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    font-size: 20px;
+  }
 `;
 
 export default Error;
