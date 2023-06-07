b0af169bc96dc580897332d085b17e44080e11c4
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-07 16:30:13 +0300

    Moved common logic of AllPosts and MyPosts to Posts component

--- ui/src/pages/AllPosts.jsx
+++ ui/src/pages/AllPosts.jsx
@@ -1,70 +1,7 @@
-import { useEffect, useState } from 'react';
-import { fetchPosts } from '../api/api';
-import PostPreview from '../components/PostPreview';
-import Pagination from '../components/Pagination';
-import { useNavigate } from 'react-router-dom';
+import Posts from '../components/Posts';
 
 const AllPosts = () => {
-  const PAGE_SIZE = 5;
-  const [posts, setPosts] = useState([]);
-  const [pageCount, setPageCount] = useState(1);
-  const [page, setPage] = useState(1);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    fetchPostPage(page);
-  }, [page]);
-
-  function fetchPostPage(pageNumber) {
-    fetchPosts({ size: PAGE_SIZE, page: pageNumber }).then((posts) => {
-      if (posts.status) {
-        navigate('/error');
-      } else {
-        setPageCount(posts.pageCount);
-        setPosts(posts.posts);
-      }
-    });
-  }
-
-  function getPosts() {
-    if (posts.length > 0) {
-      return posts.map((post) => (
-        <PostPreview key={`post_${post.postId}`} {...getPostProps(post)} />
-      ));
-    }
-  }
-
-  function onPageChange(pageNumber) {
-    setPage(pageNumber);
-  }
-
-  function getPostProps(post) {
-    return {
-      postId: post.postId,
-      userName: `${post.user.firstName} ${post.user.lastName}`,
-      userId: post.userId,
-      title: post.title,
-      content: post.content,
-      created: post.created,
-      modified: post.modified,
-      commentsCount: post._count.comments,
-    };
-  }
-
-  function getPaginationProps() {
-    return {
-      pageCount: pageCount,
-      pageRangeDisplayed: PAGE_SIZE,
-      onPageChange: (pageNumber) => onPageChange(pageNumber),
-    };
-  }
-
-  return (
-    <div className="flex-column p-10">
-      {getPosts()}
-      {pageCount > 1 && <Pagination {...getPaginationProps()} />}
-    </div>
-  );
+  return <Posts isMyPosts={false} />;
 };
 
 export default AllPosts;
--- ui/src/pages/MyPosts.jsx
+++ ui/src/pages/MyPosts.jsx
@@ -1,127 +1,18 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { fetchUserPosts, savePost } from '../api/api';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { createContext, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 import { UserContext } from '../App';
-import Editor from '../components/Editor';
-import PostPreview from '../components/PostPreview';
-import Pagination from '../components/Pagination';
-import styled from 'styled-components';
+import Posts from '../components/Posts';
 
 export const EditorContext = createContext(undefined);
 
 const MyPosts = () => {
-  const PAGE_SIZE = 5;
   const { user } = useContext(UserContext);
-  const [posts, setPosts] = useState([]);
-  const [pageCount, setPageCount] = useState(1);
-  const [page, setPage] = useState(1);
-  const [isEditorVisible, setEditorVisible] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (user !== undefined) {
-      fetchPostPage(page);
-    }
-  }, [page]);
-
-  function fetchPostPage(pageNumber) {
-    fetchUserPosts(user.userId, { size: PAGE_SIZE, page: pageNumber }).then((posts) => {
-      if (posts.status) {
-        navigate('/error');
-      } else {
-        setPageCount(posts.pageCount);
-        setPosts(posts.posts);
-      }
-    });
-  }
-
-  function onPageChange(pageNumber) {
-    setPage(pageNumber);
-  }
-
-  function getPostProps(post) {
-    return {
-      postId: post.postId,
-      userName: `${user.firstName} ${user.lastName}`,
-      userId: post.userId,
-      title: post.title,
-      content: post.content,
-      created: post.created,
-      modified: post.modified,
-      commentsCount: post._count.comments,
-      setPosts: (value) => setPosts(value),
-    };
-  }
-
-  function getEditorProps() {
-    return {
-      onSave: (title, content) => onPostSave(content, title),
-      onCancel: () => setEditorVisible(false),
-      initialTitle: '',
-      initialContent: '',
-      useTitle: true,
-    };
-  }
-
-  function getPaginationProps() {
-    return {
-      pageCount: pageCount,
-      pageRangeDisplayed: PAGE_SIZE,
-      onPageChange: (pageNumber) => onPageChange(pageNumber),
-    };
-  }
-
-  function getPosts() {
-    if (posts.length > 0) {
-      return posts.map((post) => (
-        <PostPreview key={`post_${post.postId}`} {...getPostProps(post)} />
-      ));
-    }
-  }
-
-  function onAddPostClick() {
-    setEditorVisible(true);
-  }
-
-  function onPostSave(title, content) {
-    const post = {
-      userId: user.userId,
-      title: title,
-      content: content,
-    };
-    savePost(post).then(async (res) => {
-      if (res.status === 201) {
-        fetchPostPage(page);
-        setPage(1);
-        setEditorVisible(false);
-      } else {
-        alert('An error occurred please try again later');
-      }
-    });
-  }
 
   if (user === undefined) {
     return <Navigate to="/login" />;
   }
 
-  return (
-    <div className="flex-column p-10">
-      {getPosts()}
-      {pageCount > 1 && <Pagination {...getPaginationProps()} />}
-      {isEditorVisible && <Editor {...getEditorProps()} />}
-      <EditorWrapper>
-        {!isEditorVisible && <button onClick={onAddPostClick}>Create post</button>}
-      </EditorWrapper>
-    </div>
-  );
+  return <Posts isMyPosts={true} />;
 };
-
-const EditorWrapper = styled.div.attrs({
-  className: 'flex-column'
-})`
-  justify-content: center;
-  align-content: center;
-  flex-wrap: wrap;
-`;
 
 export default MyPosts;
