324648c0bf2ae5d9469ca6e2d0c222b1d750733f
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-27 11:00:48 +0300

    Was separated post from page to dedicated component FullPost Added own routing for AllPost page with automatic redirect from root

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,46 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import { Suspense } from 'react';
 import MainLayout from './layouts/MainLayout';
 import MyPosts from './pages/MyPosts';
 import Post from './pages/Post';
 import AllPosts from './pages/AllPosts';
 import Register from './pages/Register';
-import Loading from './components/Loading';
 import { UserContextProvider } from './context/UserContext';
 
 function App() {
-
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <UserContextProvider>
-            <MainLayout />
-          </UserContextProvider>
-        }>
-        <Route path="/" element={<AllPosts />} />
-        <Route path="/user/:userId/post" element={<MyPosts />} />
-        <Route
-          path={'/user/:userId/post/:postId'}
-          element={
-            <Suspense fallback={<Loading />}>
-              <Post />
-            </Suspense>
-          }
-        />
-      </Route>
-      <Route
-        path="/login"
-        element={
-          <UserContextProvider>
-            <Login />
-          </UserContextProvider>
-        }
-      />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/post" replace />} />
+          <Route path="/post" element={<AllPosts />} />
+          <Route path="/user/:userId/post" element={<MyPosts />} />
+          <Route path="/user/:userId/post/:postId" element={<Post />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </UserContextProvider>
   );
 }
 
--- ui/src/pages/Post.jsx
+++ ui/src/pages/Post.jsx
@@ -1,105 +1,12 @@
-import { Suspense, useEffect, useRef, useState } from 'react';
-import Editor from '../components/Editor';
-import { getDate, isTheSameUser } from '../utils/utils';
-import { useParams } from 'react-router-dom';
-import CommentList from '../components/CommentList';
-import usePostQuery from '../hooks/usePostQuery';
-import usePostAdditionalParams from '../hooks/usePostAdditionalParams';
+import { Suspense } from 'react';
 import Loading from '../components/Loading';
-import useUserContext from '../hooks/useUserContext';
+import FullPost from '../components/FullPost';
 
 const Post = () => {
-  const { user: loggedInUser } = useUserContext();
-  const { userId, postId } = useParams();
-  const [editMode, setEditMode] = useState(false);
-  const { showComments, showCommentsSearch } = usePostAdditionalParams();
-  const { post, editPost, deletePost } = usePostQuery(postId, userId);
-  const { title, content, created, modified, user, commentCount: count } = post;
-  const [commentCount, setCommentCount] = useState(0);
-  const hasComments = commentCount > 0;
-  const isEditable = isTheSameUser(loggedInUser, userId);
-  const ref = useRef();
-
-  useEffect(() => {
-    setEditMode(false);
-    setCommentCount(count);
-  }, [post]);
-
-  function onPostEdit(content, title) {
-    const post = {
-      userId: loggedInUser.userId,
-      title: title,
-      content: content,
-    };
-    editPost(post);
-  }
-
-  const postElement = (
-    <>
-      <h3>{title}</h3>
-      <div className="info">
-        <span>
-          Posted by {user?.firstName} {user?.lastName}, {getDate(created)}
-        </span>
-        {modified && <span>Edited {getDate(modified)}</span>}
-      </div>
-      <span className="content">{content}</span>
-      <div className="info" style={{ textAlign: 'right' }}>
-        <div>
-          <span
-            className={hasComments ? 'action-link' : ''}
-            onClick={hasComments ? () => ref.current.setCommentsSearchParam(!showComments) : () => {}}
-            aria-disabled={hasComments}
-            style={hasComments ? {} : { textDecoration: 'none' }}>
-            Comments: {commentCount}
-          </span>
-          {showComments && hasComments && (
-            <>
-              <span>, </span>
-              <span
-                className="action-link"
-                onClick={() => ref.current.setSearch(!showCommentsSearch)}>
-                {showCommentsSearch ? 'Hide' : 'Show'} search
-              </span>
-            </>
-          )}
-        </div>
-        {isEditable && (
-          <div className="flex-row-left" style={{ flexBasis: 'auto' }}>
-            <span className="action-link" onClick={() => setEditMode(true)}>
-              Edit
-            </span>
-            <span
-              className="action-link"
-              onClick={() => deletePost(postId)}
-              style={{ marginLeft: '5px' }}>
-              Delete
-            </span>
-          </div>
-        )}
-      </div>
-      <Suspense fallback={<Loading />}>
-        <CommentList ref={ref}
-          {...{ showComments, showCommentsSearch, commentCount, setCommentCount }}
-        />
-      </Suspense>
-    </>
-  );
-
   return (
-    <div className="flex-column" style={{ padding: '10px' }}>
-      {editMode ? (
-        <Editor
-          onSave={(content, title) => onPostEdit(content, title)}
-          onCancel={() => setEditMode(false)}
-          initialTitle={title}
-          initialContent={content}
-          useTitle={true}
-        />
-      ) : (
-        postElement
-      )}
-    </div>
+    <Suspense fallback={<Loading />}>
+      <FullPost />
+    </Suspense>
   );
 };
 
