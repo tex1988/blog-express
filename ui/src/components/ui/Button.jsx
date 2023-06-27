ade0a2324af8bd045774495216675a13c4b0b7b5
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-27 18:42:46 +0300

    Added Button element Fixed SearchInput

---
+++ ui/src/components/ui/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Spinner from './Spinner';
+
+const Button = ({
+  label,
+  onClick = () => {},
+  disabled = false,
+  wait = false,
+  waitLabel = null,
+}) => {
+  return (
+    <button onClick={onClick} disabled={disabled}>
+      <div className="flex-row-center">
+        {wait && (
+          <span style={{ marginRight: '5px' }}>
+            <Spinner size="14px" color="#b2b2b2" backgroundColor="transparent" border="1px" />
+          </span>
+        )}
+        {wait && waitLabel ? waitLabel : label}
+      </div>
+    </button>
+  );
+};
+
+export default Button;
--- ui/src/components/ui/SearchInput.jsx
+++ ui/src/components/ui/SearchInput.jsx
@@ -23,10 +23,6 @@ const SearchInput = (props) => {
   }
 
   function onSearchInput(event) {
-    const value = event.target.value;
-    if (value.length === 0) {
-      onSearch(null);
-    }
     onInput(event.target.value);
   }
 
@@ -55,7 +51,7 @@ const SearchInput = (props) => {
         aria-hidden="true">
         <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path>
       </svg>
-      <button onClick={onSearchClick}>
+      <button onClick={onSearchClick} disabled={value.length < 1}>
         <svg
           viewBox="64 64 896 896"
           focusable="false"
@@ -90,6 +86,10 @@ const SearchInputWrapper = styled.div`
 
     &:hover:enabled {
       border-color: #4f79ff;
+    }
+
+    &:hover:enabled:active {
+      border-color: #3454B4;
     }
 
     svg {
