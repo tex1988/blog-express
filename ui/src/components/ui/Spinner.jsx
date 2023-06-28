4487b4e18e3bd330c1a93dedace472ac7c2ebfb5
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-28 10:53:23 +0300

    Added speed prop for Spinner

--- ui/src/components/ui/Spinner.jsx
+++ ui/src/components/ui/Spinner.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Spinner = ({ size = '20px', color = 'white', backgroundColor = 'grey', border = '3px' }) => {
-  return <SpinnerWrapper size={size} color={color} backgroundColor={backgroundColor} border={border} />
+const Spinner = ({
+  size = '20px',
+  color = 'white',
+  backgroundColor = 'grey',
+  border = '3px',
+  speed = '1s',
+}) => {
+
+  return (
+    <SpinnerWrapper
+      size={size}
+      color={color}
+      backgroundColor={backgroundColor}
+      border={border}
+      speed={speed}
+    />
+  );
 };
 
 const SpinnerWrapper = styled.div`
@@ -11,7 +26,7 @@ const SpinnerWrapper = styled.div`
   border-top: ${(props) => props.border} solid ${(props) => props.color};
   width: ${(props) => props.size};
   height: ${(props) => props.size};
-  animation: spin 1s linear infinite;
+  animation: spin ${(props) => props.speed} linear infinite;
 
   @keyframes spin {
     0% {
