4648f2ae0fd02b5d60111182676465ea577c28c3
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-28 10:48:58 +0300

    Minor Button adjustments

--- ui/src/components/ui/Button.jsx
+++ ui/src/components/ui/Button.jsx
@@ -5,18 +5,18 @@ const Button = ({
   label,
   onClick = () => {},
   disabled = false,
-  wait = false,
-  waitLabel = null,
+  loading = false,
+  loadingLabel = null,
 }) => {
   return (
     <button onClick={onClick} disabled={disabled}>
       <div className="flex-row-center">
-        {wait && (
+        {loading && (
           <span style={{ marginRight: '5px' }}>
             <Spinner size="14px" color="#b2b2b2" backgroundColor="transparent" border="1px" />
           </span>
         )}
-        {wait && waitLabel ? waitLabel : label}
+        {loading && loadingLabel ? loadingLabel : label}
       </div>
     </button>
   );
