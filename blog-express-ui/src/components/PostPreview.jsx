159a5b22f946dbbc9335596913ea5fc03dbbc4b1
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-06-30 14:06:31 +0300

    Replaced h3 with Link in PostPreview

--- blog-express-ui/src/components/PostPreview.jsx
+++ blog-express-ui/src/components/PostPreview.jsx
@@ -1,12 +1,11 @@
 import { getDate } from '../utils/utils';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const PostPreview = ({ post }) => {
   const { postId, title, content, userId, created, modified } = post;
   const commentsCount = post?._count?.comments;
   const userName = `${post?.user?.firstName} ${post?.user?.lastName}`;
   const contentPreview = getContentPreview();
-  const navigate = useNavigate();
 
   function getContentPreview() {
     let preview = content;
@@ -16,15 +15,9 @@ const PostPreview = ({ post }) => {
     return preview;
   }
 
-  function navigateToPost() {
-    navigate(`/user/${userId}/post/${postId}`);
-  }
-
   return (
     <div className="flex-column">
-      <h3 onClick={navigateToPost} style={{ cursor: 'pointer', width: 'fit-content' }}>
-        {title}
-      </h3>
+      <Link className='title' to={`/user/${userId}/post/${postId}`}>{title}</Link>
       <div className="flex-row-left info">
         <span>
           Posted by {userName}, {getDate(created)}
