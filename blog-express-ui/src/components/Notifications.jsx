6858240a0f67ee48b5d57046f25ac3cadcb2f91b
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-07-07 18:25:17 +0300

    Added framer-motion dependency Added animations for toast notifications Adjusted notification layout

--- blog-express-ui/src/components/Notifications.jsx
+++ blog-express-ui/src/components/Notifications.jsx
@@ -2,26 +2,39 @@ import React from 'react';
 import useNotificationContext from '../hooks/useNotificationContext';
 import Toast from './ui/Toast';
 import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const NOTIFICATION_DURATION = 2000;
+const NOTIFICATION_DURATION = 3000;
 
 const Notifications = () => {
   const { notifications, closeNotification } = useNotificationContext();
 
   function getToasts() {
-    return notifications.map((notification) => {
-      const { message, type } = notification;
-      return (
-        <div key={`notification_${notification.id}`} className='flex-row-center'>
-          <Toast
-            message={message}
-            type={type}
-            onClose={() => closeNotification(notification.id)}
-            duration={notification.autoClose ? NOTIFICATION_DURATION : 0}
-          />
-        </div>
-      );
-    });
+    return (
+      <ul>
+        <AnimatePresence>
+          {notifications.map((notification) => {
+            const { message, type } = notification;
+            return (
+              <motion.li
+                key={`notification_${notification.id}`}
+                layout
+                className="flex-row-center"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, x: 150, transition: { duration: 0.3 } }}>
+                <Toast
+                  message={message}
+                  type={type}
+                  onClose={() => closeNotification(notification.id)}
+                  duration={notification.autoClose ? NOTIFICATION_DURATION : 0}
+                />
+              </motion.li>
+            );
+          })}
+        </AnimatePresence>
+      </ul>
+    );
   }
 
   return <NotificationsWrapper>{getToasts()}</NotificationsWrapper>;
@@ -32,13 +45,19 @@ const NotificationsWrapper = styled.div.attrs({
 })`
   align-content: center;
   flex-wrap: wrap;
+  width: fit-content;
   left: 0;
   right: 0;
+  top: 5px;
   margin-left: auto;
   margin-right: auto;
   position: fixed;
-  top: 5px;
   z-index: 100;
+  
+  ul {
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 export default Notifications;
