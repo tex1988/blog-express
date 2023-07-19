6d6f6abb6976c029a7356711c60c13e5d2d83b61
Oleksii.Zinkevych <ozink@softserveinc.com>
2023-07-19 18:40:04 +0300

    Added animation for active link in navbar

--- blog-express-ui/src/layouts/Header.jsx
+++ blog-express-ui/src/layouts/Header.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import UserMenu from '../components/UserMenu';
 import styled from 'styled-components';
 import useAuthContext from '../hooks/useAuthContext';
+import { basicTransition } from '../components/animation/motionComponents';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   const { user } = useAuthContext();
@@ -10,6 +12,7 @@ const Header = () => {
   const userName = user ? user.username : 'Sign in';
   const ref = useRef(null);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     document.addEventListener('click', handleClickOutside, true);
@@ -37,11 +40,33 @@ const Header = () => {
     <HeaderWrapper>
       <nav>
         <div className="nav-group">
-          <NavLink className="logo" to="/">
+          <Link className="logo" to="/">
             BLOG-EXPRESS
-          </NavLink>
-          {user && <NavLink to={`/user/${user.userId}/post`}>My posts</NavLink>}
-          <NavLink to="/post">All posts</NavLink>
+          </Link>
+          {user && (
+            <div>
+              <Link to={`/user/${user.userId}/post`}> My posts</Link>
+              {location.pathname === `/user/${user.userId}/post` && (
+                <motion.div
+                  key={'nav1'}
+                  layoutId={'nav'}
+                  transition={basicTransition}
+                  className="underline"
+                />
+              )}
+            </div>
+          )}
+          <div>
+            <Link to="/post">All posts</Link>
+            {location.pathname === '/post' && (
+              <motion.div
+                key={'nav2'}
+                layoutId={'nav'}
+                transition={basicTransition}
+                className="underline"
+              />
+            )}
+          </div>
         </div>
         <div className="user-info" ref={ref}>
           <span onClick={toggleMenuVisibility}>{userName}</span>
@@ -84,6 +109,15 @@ const HeaderWrapper = styled.header.attrs({
   .active {
     border-bottom: 3px solid RoyalBlue;
     color: white;
+  }
+
+  .underline {
+    position: relative;
+    top: -3px;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: RoyalBlue;
   }
 
   .sticky {
