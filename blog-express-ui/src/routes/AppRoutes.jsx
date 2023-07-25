11a887b9fbf479f1e99636fbdb3e089c8c45acd1
ozink <zinkevich-as@ukr.net>
2023-07-25 17:04:31 +0300

    Removed AnimatePresence from AppRouteComponent as it doesn't work properly with useNavigate function

--- blog-express-ui/src/routes/AppRoutes.jsx
+++ blog-express-ui/src/routes/AppRoutes.jsx
@@ -7,31 +7,28 @@ import MyPosts from '../pages/MyPosts';
 import Post from '../pages/Post';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import { AnimatePresence } from 'framer-motion';
 
 function AppRoutes() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode='wait'>
-      <Routes key={location.pathname} location={location}>
-        <Route path='/' element={<MainLayout />}>
-          <Route path='/' element={<Navigate to='/post' replace />} />
-          <Route path='/post' element={<AllPosts />} />
-          <Route
-            path='/user/:userId/post'
-            element={
-              <RequireAuth>
-                <MyPosts />
-              </RequireAuth>
-            }
-          />
-          <Route path='/user/:userId/post/:postId' element={<Post />} />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </AnimatePresence>
+    <Routes key={location.pathname} location={location}>
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Navigate to='/post' replace />} />
+        <Route path='/post' element={<AllPosts />} />
+        <Route
+          path='/user/:userId/post'
+          element={
+            <RequireAuth>
+              <MyPosts />
+            </RequireAuth>
+          }
+        />
+        <Route path='/user/:userId/post/:postId' element={<Post />} />
+      </Route>
+      <Route key={'/login'} path='/login' element={<Login />} />
+      <Route key={'/register'} path='/register' element={<Register />} />
+    </Routes>
   );
 }
 
